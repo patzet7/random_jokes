0cbc4b5420d141bebdf33cc3399d9812c048285a
patrysia <patrycja@example.com>
2017-07-03 21:24:03 +0200

    Added files: index.html, scripts.js and style.css

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var url = 'http://api.icndb.com/jokes/random';
+
+var button = document.getElementById('get-joke');
+	
+	button.addEventListener('click', function(){
+  	getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
+
+$(document).ready(getJoke);
